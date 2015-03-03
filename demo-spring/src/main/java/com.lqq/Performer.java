f3d92bfa4ed6dcc3d2496d9f3462d31f28ca370a
liuqq123 <muchuanj@foxmail.com>
2015-03-03 21:59:08 +0800

    add spring proj

---
+++ demo-spring/src/main/java/com.lqq/Performer.java
@@ -0,0 +1,8 @@
+package com.lqq;
+
+/**
+ * Created by Administrator on 2015/3/3.
+ */
+public interface Performer {
+    void perform();
+}
---
+++ demo-spring/src/main/java/com.lqq/impl/Juggler.java
@@ -0,0 +1,22 @@
+package com.lqq.impl;
+
+import com.lqq.Performer;
+
+/**
+ * Created by Administrator on 2015/3/3.
+ */
+public class Juggler implements Performer {
+    private int beanBags = 3;
+
+    public Juggler() {
+    }
+
+    public Juggler(int beanBags) {
+        this.beanBags = beanBags;
+    }
+
+    @Override
+    public void perform() {
+        System.out.println("JUGGLING " + beanBags + " BEANBAGS");
+    }
+}
